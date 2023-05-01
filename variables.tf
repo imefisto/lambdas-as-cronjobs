9d643b28f6b111aa1dc6a93beca1575ac033671c
Ignacio El Kadre <imefisto@gmail.com>
2023-05-01 08:30:47 -0300

    define variables

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  type = string
+}
+
+variable "generate_report_function_name" {
+  default = "generate-report"
+}
+
+variable "generate_report_host" {
+  type = string
+}
+
+variable "generate_report_endpoint" {
+  type = string
+}
